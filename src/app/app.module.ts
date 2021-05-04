19f7da943fc1ba41811d9bbcb929720d5249660e
Dilip Duraiswamy <dilipduraiswamy@gmail.com>
2021-05-04 23:26:53 +0530

    updated code with dynamic design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditAppComponent } from './edit-app/edit-app.component';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { DndModule } from 'ngx-drag-drop';
+import { SweetAlert2Module } from '@toverux/ngx-sweetalert2';
+const appRoutes: Routes = [
+  { path: '', component: EditAppComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditAppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes),
+    SweetAlert2Module.forRoot(),
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    DndModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
